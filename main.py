d52bb382d73e613444c068c9e84190a97d9740dd
Juan Cruz <jc23729@gmail.com>
2021-02-18 03:51:55 +0000

    update

--- main.py
+++ main.py
@@ -21,3 +21,6 @@ for letter in chosen_word:
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
