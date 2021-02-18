962706585312228b646cf96dd9f04b5dce0fc10a
Juan Cruz <jc23729@gmail.com>
2021-02-18 03:51:32 +0000

    update

--- main.py
+++ main.py
@@ -15,3 +15,9 @@ for letter in chosen_word:
         print("Right")
     else:
         print("Wrong")
+
+#Step 2
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
