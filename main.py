41060685a740332b49f271b51874a5e08e10c839
Juan Cruz <jc23729@gmail.com>
2021-02-18 03:52:25 +0000

    create empty list called display

--- main.py
+++ main.py
@@ -24,3 +24,11 @@ chosen_word = random.choice(word_list)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
+
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
